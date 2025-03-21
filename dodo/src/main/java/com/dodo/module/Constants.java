740cf672b3bbf08d733e0f547528cf5c0e80885a
Minju-1017 <mjk373737@gmail.com>
2025-03-21 13:32:36 +0900

    벨리데이션

--- dodo/src/main/java/com/dodo/module/Constants.java
+++ dodo/src/main/java/com/dodo/module/Constants.java
@@ -2,11 +2,15 @@ package com.dodo.module;
 
 import java.text.SimpleDateFormat;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Constants {
 	
 	public final static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
 	public final static SimpleDateFormat DATETIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	
-	public final static int CODE_GROUP_SEQ_MEMBER_GENDER = 1; // Member(mGenderCd)에 사용하는 코드 그룹
+	// Member에 사용하는 코드 그룹
+	public final static int MEMBER_CODE_GROUP_SEQ_GENDER = 1; // mGenderCd
 
 }
